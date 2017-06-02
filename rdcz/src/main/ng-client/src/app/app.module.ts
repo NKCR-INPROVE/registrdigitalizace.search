4885a6f32c831287f8bbef6db9051fa5983babd4
samba <pkudela@searchtechnologies.com>
2017-06-02 15:49:41 +0200

    Angular, sass, route, components configuration

--- rdcz/src/main/ng-client/src/app/app.module.ts
+++ rdcz/src/main/ng-client/src/app/app.module.ts
@@ -2,17 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { MaterializeModule } from 'ng2-materialize'; // _app
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    HeaderComponent,
+    FooterComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterializeModule.forRoot(), // _app
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'search', component: SearchComponent },
+      { path: 'info', component: InfoComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
