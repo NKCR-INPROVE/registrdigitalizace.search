aa1a2bc8169f0bf559acf2dd16ea34e86a2f86c9
Alberto Hernandez <alberto@albertoX1>
2017-07-10 12:40:31 +0200

    automatic closing adv search

--- rdcz/src/main/ng-client/src/app/app.module.ts
+++ rdcz/src/main/ng-client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -77,7 +77,6 @@ export function createTranslateLoader(http: Http) {
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpModule,
     TranslateModule.forRoot({
             loader: {
