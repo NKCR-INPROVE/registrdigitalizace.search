27ad673e67dbea085a0da0c0bb35af02f7a31b86
samba <pkudela@searchtechnologies.com>
2017-07-20 14:19:53 +0200

    Home page - owner facet - sorting - active ico, static example for list dk

--- rdcz/src/main/ng-client/src/app/components/facets-home/facets-home.component.ts
+++ rdcz/src/main/ng-client/src/app/components/facets-home/facets-home.component.ts
@@ -1,15 +1,137 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { Subscription } from 'rxjs/Subscription';
+
+import { FacetField } from '../../models/facet-field';
+import { Facet } from '../../models/facet';
+import { Filter } from '../../models/filter';
+import { AppState } from '../../app.state';
+import { AppService } from '../../app.service';
 
 @Component({
   selector: 'app-facets-home',
   templateUrl: './facets-home.component.html',
   styleUrls: ['./facets-home.component.scss']
 })
-export class FacetsHomeComponent implements OnInit {
+export class FacetsHomeComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() facetFields: FacetField[] = [];
+  @Input() page = "results";
+  @Input() allClosed = true;
+  @Input() allOpen = true;
+  subscriptions: Subscription[] = [];
+   
+   
+    currentSort: string= 'human';
+    currentDir: number = 1;
+
+  constructor(public service: AppService, public state: AppState) { }
 
   ngOnInit() {
+    //    
+    //    this.subscriptions.push(this.state.searchSubject.subscribe(
+    //      (resp) => {
+    //        //console.log(resp);
+    //        if (resp['type'].indexOf('results') > -1) {
+    //          if (resp['state'] === 'start') {
+    //            this.facetFields = [];
+    //          } else {
+    //            this.fillFacets(this.state.facets);
+    //          }
+    //        }
+    //      }
+    //    ));
   }
 
+  ngOnDestroy() {
+    this.subscriptions.forEach((s: Subscription) => {
+      s.unsubscribe();
+    });
+    this.subscriptions = [];
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+//    console.log(changes);
+
+    //    if (changes['facetFields']){
+    ////      this.view = true;
+    //    }
+  }
+
+  add(f: Facet) {
+    let filter: Filter = new Filter();
+    filter.field = f.field;
+    filter.value = f.value;
+    this.state.addFilter(filter);
+    this.service.goToResults();
+  }
+
+  translate(ff: FacetField, f: Facet) {
+    if (ff.translate) {
+      if (ff.classname) {
+        if(f['human']){
+          return f['human'];
+        } else {
+          let human = this.service.translateFromLists(ff.classname, f.value);
+          f['human'] = human;
+          return human;
+        }
+      } else {
+        return f.value;
+      }
+    } else {
+      return f.value;
+    }
+    }
+
+//  facetUsed(field: string, value: string)   {
+//    for (let f in this.filters)   {
+//      let name = this.filters[f].field + '  ';
+//      if (name === field)   {
+//        if (this.filters[f].isMultiple)   {
+//          for (let i = 0; i < this.filters[f].values.length; i++)   {
+//            if (value === this.filters[f].values[i].displayValue)   {
+//              return tru  e;
+//              }
+//            }
+//        } else   {
+//          if (value === this.filters[f].displayValue)   {
+//            return tru  e;
+//            }
+//          }
+//        }
+//      }
+//    return fals  e;
+//  }
+
+  // state facets are visible
+  isVisible(i: number, ff: FacetField) {
+    if (i < 5) {
+      return true;
+    }
+    if (ff.hasOwnProperty('expanded')) {
+      return ff['expanded'];
+    }
+    else {
+      return false;
+    }
+  }
+
+  // click and show more facets
+  toggleMoreFacets(ff: FacetField) {
+    if (ff.hasOwnProperty('expanded')) {
+      ff['expanded'] = !ff['expanded'];
+    }
+    else {
+      ff['expanded'] = true;
+    }
+  }
+  
+  sortBy(ff: FacetField, col: string){
+    this.currentSort= col;
+    this.currentDir = - this.currentDir;
+    ff.values.sort((a,b) => {
+      return a[col] > b[col] ? 1 : -1;
+    });
+  }
 }
