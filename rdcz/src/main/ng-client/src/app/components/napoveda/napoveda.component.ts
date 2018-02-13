8fb9aa6b15f805f109ef20384b5a997bad983f15
Alberto Hernandez <alberto@albertoX1>
2018-02-13 14:56:40 +0100

    #591

--- rdcz/src/main/ng-client/src/app/components/napoveda/napoveda.component.ts
+++ rdcz/src/main/ng-client/src/app/components/napoveda/napoveda.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, ComponentFactoryResolver} from '@angular/core';
+import {Router, ActivatedRoute, NavigationEnd} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import {AppState} from '../../app.state';
+import {AppService} from '../../app.service';
+import {InnerContentComponent} from '../inner-content/inner-content.component';
 
 @Component({
   selector: 'app-napoveda',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NapovedaComponent implements OnInit {
 
-  constructor() { }
+  subscriptions: Subscription[] = [];
+  @ViewChild(InnerContentComponent) inn: InnerContentComponent;
+  constructor(
+    public state: AppState,
+    public service: AppService) {}
 
   ngOnInit() {
+    this.subscriptions.push(this.service.langSubject.subscribe(
+      () => {
+
+        this.service.getText('pages/help').subscribe(t => {
+          this.inn.setText(t);
+        });
+      }
+    ));
+
   }
 
 }
